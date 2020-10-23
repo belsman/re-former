8416b20012d295dcbd5d941f48d55b69f4b74e54
Bello S Babakolo <fleetbeekay@gmail.com>
2020-10-23 18:20:18 +0100

    Implement Edit User feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     include UsersHelper
     
     def new
-        @user = User.new(username: "kazee", email: "kas@example.com", password: "password")
+        @user = User.new
     end
 
     def create
@@ -11,6 +11,20 @@ class UsersController < ApplicationController
             redirect_to new_user_path
         else
             render :new
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        if @user.save
+            redirect_to edit_user_path
+        else
+            render :edit
         end
     end
 end
