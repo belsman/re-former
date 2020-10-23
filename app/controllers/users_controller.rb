ffbdc9123fe9e64c3fe1e52a3cf121bdf1e5f71a
Bello S Babakolo <fleetbeekay@gmail.com>
2020-10-23 15:54:44 +0100

    Implement forms with form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+    include UsersHelper
+    
     def new
+        @user = User.new(username: "kazee", email: "kas@example.com", password: "password")
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
         if @user.save
             redirect_to new_user_path
         else
